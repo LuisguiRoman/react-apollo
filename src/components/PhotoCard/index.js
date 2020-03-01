0ad6ff6ec20b6a46954558c08d238bf82a3bccdc
Luisgui <luisguirohi@gmail.com>
2020-03-01 16:13:06 -0500

    polyfill para lazy load

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -8,18 +8,24 @@ const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_150/v15
 export const PhotoCard = ({id, src = DEFAULT_IMAGE, likes = 0}) => {
     //state
     const [show, setShow] = useState(false);
-
+    //ref
     const element = useRef(null);
 
     useEffect(()=>{
-        const observer = new IntersectionObserver(entries=>{
-            const { isIntersecting } = entries[0];
-            if(isIntersecting){
-                setShow(true);
-                observer.disconnect();
-            }
-        });
-        observer.observe(element.current);
+        //polifill de para cuando un navegador lo requiera
+        Promise.resolve(
+            typeof window.IntersectionObserver !== 'undefined' ? 
+            window.IntersectionObserver : import('intersection-observer')
+        ).then(()=>{
+            const observer = new IntersectionObserver(entries=>{
+                const { isIntersecting } = entries[0];
+                if(isIntersecting){
+                    setShow(true);
+                    observer.disconnect();
+                }
+            });
+            observer.observe(element.current);
+        })
     }, [element]);
 
     return(
