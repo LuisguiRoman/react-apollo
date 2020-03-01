c50a28b63f9d3dad4f635ef30cab225b0f33cb2e
Luisgui <luisguirohi@gmail.com>
2020-03-01 13:08:25 -0500

    keyframes y mixins con styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GlobalStyle } from './GlobalStyles';
+import { GlobalStyle } from './styles/GlobalStyles';
 import { Logo } from './components/Logo';
 import { ListOfCategories } from './components/ListOfCategories';
 import { ListOfPhotoCards } from './components/ListOfPhotoCards';
--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { fadeIn } from '../../styles/animations';
+
 
 export const Figure = styled.figure`
     border-radius: 10px;
@@ -11,6 +13,7 @@ export const Figure = styled.figure`
 `;
 
 export const Image = styled.img`
+    ${fadeIn('1s', 'ease-in-out')};
     box-shadow: 0px 10px 14px rgba(0,0,0,.2);
     height: 100%;
     object-fit: cover;
