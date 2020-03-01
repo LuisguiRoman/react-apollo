a0e8ff4346760ee5a9e1298b0624cabf1b4eb506
Luisgui <luisguirohi@gmail.com>
2020-03-01 17:16:40 -0500

    Instalacion de apollo y creacion de provider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import { App } from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//constants
+import { SERVER_URL } from './constants';
+
+//crear un provider con la api de graphql
+const client = new ApolloClient({
+    url: `${SERVER_URL}/graphql`
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>, 
+    document.getElementById('root')
+);
